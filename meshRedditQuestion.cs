884cab9a2f89efd9a4a218229592ac4d21da2872
HeRobrain3 <153457341+HeRobrain3@users.noreply.github.com>
2023-12-10 21:29:45 -0500

    Update meshRedditQuestion.cs

--- meshRedditQuestion.cs
+++ meshRedditQuestion.cs
@@ -13,10 +13,7 @@ public class Place : MonoBehaviour
 {
     public static Place placeInstance;
 
-    [SerializeField] GameObject wall;
-    [SerializeField] GameObject lava;
-    [SerializeField] GameObject gunTurret;
-    [SerializeField] GameObject RaycastAgent;
+    [SerializeField] GameObject RaycastAgent; //Receive the big box in Unity
 
     public List<Personnage> enn;
 
@@ -40,71 +37,16 @@ public class Place : MonoBehaviour
     public void placer(Vector3 posMouse)
     {
 
-        if (GameManager.gameManagerInstance.points != null && GameManager.gameManagerInstance.FindMouseObject("Ground") && Input.GetMouseButtonDown(0))
-        {
-           // MeshFilter meshWall;
+           //unrelated code before removed...
             MeshFilter meshRaycast;
-
-            GameObject tmp = ObjectPool.instance.GetPooledObject(wall);
-            tmp.transform.position = posMouse;
-            tmp.transform.rotation = gameObject.transform.rotation;
-            RaycastAgent.transform.position = posMouse;
-            RaycastAgent.transform.rotation = gameObject.transform.rotation;
-            tmp.SetActive(true);
-            //meshWall = tmp.GetComponent<MeshFilter>();
             meshRaycast = RaycastAgent.GetComponent<MeshFilter>();
-
-            for (int i = 0; i < GameManager.gameManagerInstance.points.GetLength(0); i++)
-            {
-                for (int j = 0; j < GameManager.gameManagerInstance.points.GetLength(0) - 1; j++)
-                {
-
-                    if (IsPointInPolygon(GetColliderVertexPositions(meshRaycast), GameManager.gameManagerInstance.points[i,j].transform.position))
-                    {
-                        GameManager.gameManagerInstance.points[i, j].GetComponent<MeshRenderer>().material = GameManager.gameManagerInstance.mat[1];
-                        GameManager.gameManagerInstance.points[i, j].Isolation();
-
-                    }else if (IsPointInPolygon(GetColliderVertexPositions(meshRaycast), GameManager.gameManagerInstance.points[i, j].transform.position))
-                    {
-                        print("nein!");
-                        GameManager.gameManagerInstance.points[i, j].RaycastCheck();
-                    }
-                }
-            }
-            enn.Sort((p1, p2) => p1.GetComponent<Personnage>().Dist(tmp.transform.position).CompareTo(p2.GetComponent<Personnage>().Dist(tmp.transform.position)));
-            foreach (Personnage p in enn)
-            {
-                p.genPath();
-            }
-
-        }
+            GetColliderVertexPositions(meshRaycast)
+          //unrelated code after removed...         
 
     }
 
 
-    public bool IsPointInPolygon(Vector3[] polygon, Vector3 point)
-    {
-        int polygonLength = polygon.Length, i = 0;
-        bool inside = false;
-        // x, y for tested point.
-        float pointX = point.x, pointY = point.z;
-        // start / end point for the current polygon segment.
-        float startX, startY, endX, endY;
-        Vector3 endPoint = polygon[polygonLength - 1];
-        endX = endPoint.x;
-        endY = endPoint.z;
-        while (i < polygonLength)
-        {
-            startX = endX; startY = endY;
-            endPoint = polygon[i++];
-            endX = endPoint.x; endY = endPoint.z;
-            //
-            inside ^= (endY > pointY ^ startY > pointY) /* ? pointY inside [startY;endY] segment ? */
-                      && /* if so, test if it is under the segment */
-                      ((pointX - endX) < (pointY - endY) * (startX - endX) / (startY - endY));
-        }
-        return inside;
-    }
+   
 
     public Vector3[] GetColliderVertexPositions(MeshFilter mesh)
     {
